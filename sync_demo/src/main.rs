682ee978eef9f6e8cf10eae461d56ca5c69c464d
Ole Martin Gjersvik <olgj@equinor.com>
2024-01-02 15:44:03 +0100

    Added commands to sync demo.

--- sync_demo/src/main.rs
+++ sync_demo/src/main.rs
@@ -1,3 +1,33 @@
+use std::env::{self, Args};
+
 fn main() {
-    println!("Hello, world!");
+    // Get arguments
+    let mut args = env::args();
+
+    // Remove the first one don't need to know the executable
+    args.next();
+
+    // Match on the next to decide operation
+    match args.next().as_deref() {
+        Some("list") => list(),
+        Some("add") => add(args),
+        Some("remove") => remove(args),
+        _ => help(),
+    }
+}
+
+fn help() {
+    todo!("Todo: Implement help")
+}
+
+fn remove(_args: Args) {
+    todo!("Todo: Implement remove")
+}
+
+fn add(_args: Args) {
+    todo!("Todo: Implement add")
+}
+
+fn list() {
+    todo!("Todo: Implement list")
 }
