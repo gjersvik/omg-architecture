daf8788e88457e41f757ab313524317419ab3bbd
Ole Martin Gjersvik <olgj@equinor.com>
2024-01-03 09:03:26 +0100

    Help text to blocking demo.

--- sync_demo/src/main.rs
+++ sync_demo/src/main.rs
@@ -23,7 +23,13 @@ fn main() {
 }
 
 fn help() {
-    todo!("Todo: Implement help")
+    println!("Help for Sync demo todo app");
+    println!("sync_demo list");
+    println!("Will list all the active todo items.");
+    println!("sync_demo add [task]");
+    println!("Adds task to the todo lists.");
+    println!("sync_demo remove [id]");
+    println!("Removes/completes the task with id: id.");
 }
 
 fn remove(_args: Args, _agent: Agent) {
