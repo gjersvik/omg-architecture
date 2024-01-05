318fb777e894465d7f6592d466f65fd9e24fa762
Ole Martin Gjersvik <olgj@equinor.com>
2024-01-05 15:41:17 +0100

    Added todo to all core functions.

--- omg_core/src/lib.rs
+++ omg_core/src/lib.rs
@@ -12,34 +12,29 @@ pub struct Agency {
 }
 
 impl Agency {
-    pub fn new(storage: Option<Box<dyn Storage>>) -> Agency {
-        Agency {
-            _storage: storage.map(Into::into),
-        }
+    pub fn new(_storage: Option<Box<dyn Storage>>) -> Agency {
+        todo!("Implement: Agency::new")
     }
 
     pub fn get<K: Key, V>(&self, _name: &str) -> Agent<K, V> {
-        Agent {
-            view: BTreeMap::new(),
-        }
+        todo!("Implement: Agency::get")
     }
 }
 
 pub struct Agent<K: Key, V> {
-    view: BTreeMap<K, V>,
+    _view: BTreeMap<K, V>,
 }
 
 impl<K: Key, V> Agent<K, V> {
     pub fn load_blocking(&self) -> Result<&BTreeMap<K, V>, String> {
-        Ok(&self.view)
+        todo!("Implement: Agent::load_blocking")
     }
 
-    pub fn insert_blocking(&mut self, key: K, value: V) -> Result<(), String> {
-        self.view.insert(key, value);
-        Ok(())
+    pub fn insert_blocking(&mut self, _key: K, _value: V) -> Result<(), String> {
+        todo!("Implement: Agent::insert_blocking")
     }
 
-    pub fn remove_blocking(&mut self, key: &K) -> Result<Option<V>, String> {
-        Ok(self.view.remove(key))
+    pub fn remove_blocking(&mut self, _key: &K) -> Result<Option<V>, String> {
+        todo!("Implement: Agent::insert_blocking")
     }
 }
