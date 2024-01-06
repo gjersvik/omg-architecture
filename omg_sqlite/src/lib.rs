4b152163a2e6e4300a31495c8672de620bb94129
Ole Martin Gjersvik <olgj@equinor.com>
2024-01-06 13:48:48 +0100

    Implemented read all.

--- omg_sqlite/src/lib.rs
+++ omg_sqlite/src/lib.rs
@@ -1,18 +1,18 @@
-use std::{error::Error, path::Path};
+use std::{error::Error, path::Path, sync::Arc};
 
-use omg_core::Storage;
+use omg_core::{Message, Storage};
 use serde_json::Value;
 use sqlite::{Connection, ConnectionThreadSafe};
 use time::OffsetDateTime;
 
 pub fn file_blocking(path: impl AsRef<Path>) -> Result<Box<dyn Storage>, Box<dyn Error>> {
     let db = Connection::open_thread_safe(path)?;
-    db.execute("CREATE TABLE IF NOT EXISTS messages (topic TEXT, seq, INTEGER, created INTEGER, stored INTEGER, data TEXT)")?;
-    Ok(Box::new(SqliteBackend { _db: db }))
+    db.execute("CREATE TABLE IF NOT EXISTS messages (topic TEXT, seq INTEGER, created INTEGER, stored INTEGER, data TEXT)")?;
+    Ok(Box::new(SqliteBackend { db }))
 }
 
 struct SqliteBackend {
-    _db: ConnectionThreadSafe,
+    db: ConnectionThreadSafe,
 }
 
 impl Storage for SqliteBackend {
@@ -25,7 +25,27 @@ impl Storage for SqliteBackend {
         todo!()
     }
 
-    fn read_all_blocking(&self, _topic: &str) -> Result<Vec<omg_core::Message>, Box<dyn Error>> {
-        todo!()
+    fn read_all_blocking(&self, topic: &str) -> Result<Vec<omg_core::Message>, Box<dyn Error>> {
+        let mut statement = self.db.prepare(
+            "SELECT seq, created, stored, data FROM messages WHERE topic = ? ORDER BY seq ASC",
+        )?;
+        statement.bind((1, topic))?;
+
+        let topic_name: Arc<str> = Arc::from(topic);
+
+        statement
+            .into_iter()
+            .map(|row| {
+                let row = row?;
+
+                Ok(Message {
+                    topic_name: topic_name.clone(),
+                    seq: row.try_read::<i64, _>("seq")? as u64,
+                    created: OffsetDateTime::from_unix_timestamp(row.try_read("created")?)?,
+                    stored: OffsetDateTime::from_unix_timestamp(row.try_read("stored")?)?,
+                    data: serde_json::from_str(row.try_read("data")?)?,
+                })
+            })
+            .collect()
     }
 }
