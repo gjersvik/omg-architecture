e4be06df5ab8f005bbd5b84e55b057737b66cafe
Ole Martin Gjersvik <olgj@equinor.com>
2024-01-07 12:15:27 +0100

    sync demo uses topics.

--- sync_demo/src/main.rs
+++ sync_demo/src/main.rs
@@ -4,7 +4,7 @@ use std::{
     error::Error,
 };
 
-use omg_core::Agency;
+use omg_core::{Agency, Topic};
 
 fn main() -> Result<(), Box<dyn Error>> {
     // Before the main application starts we configure the Agency using crates that implements features.
@@ -12,6 +12,7 @@ fn main() -> Result<(), Box<dyn Error>> {
     let storage = omg_sqlite::file_blocking("todo.db").unwrap();
 
     let agency = Agency::new(storage);
+    let topic = agency.topic("todo");
 
     // Get arguments
     let mut args = env::args();
@@ -21,9 +22,9 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     // Match on the next to decide operation
     match args.next().as_deref() {
-        Some("list") => list(&agency),
-        Some("add") => add(args, &agency),
-        Some("remove") => remove(args, &agency),
+        Some("list") => list(&topic),
+        Some("add") => add(args, &topic),
+        Some("remove") => remove(args, &topic),
         _ => {
             help();
             Ok(())
@@ -43,12 +44,10 @@ fn help() {
     println!("Removes/completes the task with id: id.");
 }
 
-fn remove(mut args: Args, agency: &Agency) -> Result<(), Box<dyn Error>> {
+fn remove(mut args: Args, topic: &Topic) -> Result<(), Box<dyn Error>> {
     if let Some(id) = args.next().and_then(|s| s.parse::<u64>().ok()) {
         // There we use the blocking version of the remove api. The change will be persisted before return.
-        agency.topic("todo").publish(
-            serde_json::to_value((id, None::<String>))?,
-        )?;
+        topic.publish(serde_json::to_value((id, None::<String>))?)?;
         println!("Removed task with id {id}")
     } else {
         println!("No task was provided. sync_demo remove [task]")
@@ -56,16 +55,14 @@ fn remove(mut args: Args, agency: &Agency) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn add(mut args: Args, agency: &Agency) -> Result<(), Box<dyn Error>> {
+fn add(mut args: Args, topic: &Topic) -> Result<(), Box<dyn Error>> {
     if let Some(task) = args.next() {
-        let next_id = load_tasks(agency)?
+        let next_id = load_tasks(topic)?
             .last_key_value()
             .map(|(id, _)| *id + 1)
             .unwrap_or(1);
 
-        agency
-            .topic("todo")
-            .publish(serde_json::to_value((next_id, Some(&task)))?)?;
+        topic.publish(serde_json::to_value((next_id, Some(&task)))?)?;
         println!("Added {task} with id {next_id}")
     } else {
         println!("No task was provided. sync_demo add [task]")
@@ -73,15 +70,16 @@ fn add(mut args: Args, agency: &Agency) -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
-fn list(agency: &Agency) -> Result<(), Box<dyn Error>> {
-    for (id, task) in load_tasks(agency)?.iter() {
+fn list(topic: &Topic) -> Result<(), Box<dyn Error>> {
+    for (id, task) in load_tasks(topic)?.iter() {
         println!("{id}: {task}");
     }
     Ok(())
 }
 
-fn load_tasks(agency: &Agency) -> Result<BTreeMap<u64, String>, Box<dyn Error>> {
-    let tasks = agency.topic("todo").subscribe()?
+fn load_tasks(topic: &Topic) -> Result<BTreeMap<u64, String>, Box<dyn Error>> {
+    let tasks = topic
+        .subscribe()?
         .try_fold(BTreeMap::new(), |mut map, msg| {
             let event: Result<(u64, Option<String>), _> = serde_json::from_value(msg.data);
             match event {
