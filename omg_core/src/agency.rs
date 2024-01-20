6dad6f23f55018efb62fbc45afd65ff22ea6f6f0
Ole Martin Gjersvik <olgj@equinor.com>
2024-01-20 12:07:52 +0100

    Agency basics.

--- omg_core/src/agency.rs
+++ omg_core/src/agency.rs
@@ -1,39 +1,66 @@
-use std::time::Duration;
+use std::{
+    sync::Arc,
+    time::{Duration, Instant},
+};
 
-use async_executor::Task;
+use async_executor::{Executor, Task};
 use futures_lite::Future;
 
 use crate::{Agent, ReadHandle, WriteHandle};
 
-pub struct Agency {}
+pub struct Agency {
+    ex: Arc<Executor<'static>>,
+}
 
 impl Agency {
     pub fn new() -> Self {
-        Agency {}
+        Agency {
+            ex: Arc::new(Executor::new()),
+        }
     }
 
-    pub fn add_agent(&self, _agent: impl Agent) {
-        todo!()
+    pub fn add_agent(&self, agent: impl Agent + 'static) {
+        self.spawn(async move { agent.run().await }).detach();
     }
 
-    pub fn connect<In: Clone, Out>(
-        _from: ReadHandle<In>,
-        _to: WriteHandle<Out>,
-        _filter_map: impl Fn(In) -> Option<Out>,
+    pub fn connect<In: Clone + Send + Sync + 'static, Out: Send + 'static>(
+        &self,
+        mut from: ReadHandle<In>,
+        to: WriteHandle<Out>,
+        filter_map: impl Fn(In) -> Option<Out> + Send + 'static,
     ) {
-        todo!()
+        self.spawn(async move {
+            while let Ok(msg) = from.read().await {
+                let msg = filter_map(msg);
+                if let Some(msg) = msg {
+                    let _ = to.write(msg).await;
+                }
+            }
+        })
+        .detach();
     }
 
-    pub fn spawn<T: Send>(&self, _future: impl Future<Output = T> + Send) -> Task<T> {
-        todo!()
+    pub fn spawn<T: Send + 'static>(
+        &self,
+        future: impl Future<Output = T> + Send + 'static,
+    ) -> Task<T> {
+        self.ex.spawn(future)
     }
 
     pub fn tick(&self) -> bool {
-        todo!()
+        self.ex.try_tick()
     }
 
-    pub fn tick_for(&self, _duration: Duration) {
-        todo!()
+    pub fn tick_for(&self, duration: Duration) {
+        let start = Instant::now();
+        loop {
+            if start.elapsed() >= duration {
+                break;
+            }
+            if !self.tick() {
+                break;
+            }
+        }
     }
 
     pub fn block(&self) {
