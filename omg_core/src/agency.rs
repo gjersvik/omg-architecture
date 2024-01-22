8b4b28b38604ab4a97572ecbd33424cece3af896
Ole Martin Gjersvik <olem@olem.org>
2024-01-22 06:29:37 +0100

    Agency is now a trait.

--- omg_core/src/agency.rs
+++ omg_core/src/agency.rs
@@ -1,29 +1,18 @@
-use std::{
-    sync::Arc,
-    time::{Duration, Instant},
-};
-
-use async_executor::{Executor, Task};
-use futures_lite::Future;
+use std::future::Future;
 
 use crate::{Agent, ReadHandle, WriteHandle};
 
-pub struct Agency {
-    ex: Arc<Executor<'static>>,
-}
+pub trait Agency {
+    fn spawn(
+        &self,
+        future: impl Future<Output = ()> + Send + 'static,
+    );
 
-impl Agency {
-    pub fn new() -> Self {
-        Agency {
-            ex: Arc::new(Executor::new()),
-        }
+    fn add_agent(&self, agent: impl Agent + 'static) {
+        self.spawn(async move { agent.run().await });
     }
 
-    pub fn add_agent(&self, agent: impl Agent + 'static) {
-        self.spawn(async move { agent.run().await }).detach();
-    }
-
-    pub fn connect<In: Clone + Send + Sync + 'static, Out: Send + 'static>(
+    fn connect<In: Clone + Send + Sync + 'static, Out: Send + 'static>(
         &self,
         mut from: ReadHandle<In>,
         to: WriteHandle<Out>,
@@ -37,59 +26,6 @@ impl Agency {
                 }
             }
         })
-        .detach();
-    }
-
-    pub fn spawn<T: Send + 'static>(
-        &self,
-        future: impl Future<Output = T> + Send + 'static,
-    ) -> Task<T> {
-        self.ex.spawn(future)
-    }
-
-    pub fn tick(&self) -> bool {
-        self.ex.try_tick()
-    }
-
-    pub fn tick_for(&self, duration: Duration) {
-        let start = Instant::now();
-        loop {
-            if start.elapsed() >= duration {
-                break;
-            }
-            if !self.tick() {
-                break;
-            }
-        }
-    }
-
-    pub fn block(&self) {
-        todo!()
-    }
-
-    pub fn block_for(&self, _duration: Duration) {
-        todo!()
-    }
-
-    pub fn run_background(&self) {
-        todo!()
-    }
-
-    pub fn run_background_max_cpu(&self) {
-        todo!()
-    }
-
-    pub fn full_stop(&self) {
-        todo!()
-    }
-
-    pub fn run_to_completion() {
-        todo!()
     }
 }
 
-impl Default for Agency {
-    fn default() -> Self {
-        Self::new()
-    }
-}
